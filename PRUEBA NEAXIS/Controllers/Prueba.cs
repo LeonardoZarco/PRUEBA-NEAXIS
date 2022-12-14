18a15d7242b6cb04d7af716b9e457e05c88ec946
LeonardoZarco <JLeonardoZarcoO@gmail.com>
2022-12-13 18:38:58 -0600

    Agregar archivos de proyecto.

---
+++ PRUEBA NEAXIS/Controllers/Prueba.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRUEBA_NEAXIS.Controllers
+{
+    public class Prueba : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
